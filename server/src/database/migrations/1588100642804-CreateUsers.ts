cc62b352c424e70d2a55df6530727912956abe6d
Flávio Pangrácio <flaviohugo.14@gmail.com>
2020-04-28 16:10:34 -0300

    migration: CreateUsers

--- server/src/database/migrations/1588100642804-CreateUsers.ts
+++ server/src/database/migrations/1588100642804-CreateUsers.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1587665017980
-  implements MigrationInterface {
+export default class CreateUsers1588100642804 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -15,14 +14,24 @@ export default class CreateAppointments1587665017980
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
             isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
-            isNullable: false,
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
           },
         ],
       }),
@@ -30,6 +39,6 @@ export default class CreateAppointments1587665017980
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
--- server/src/services/CreateAppoitmentService.ts
+++ server/src/services/CreateAppoitmentService.ts
@@ -1,4 +1,5 @@
 import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
@@ -8,24 +9,13 @@ interface Request {
   date: Date;
 }
 
-/**
- * SOLID
- *
- * Single Responsability Principle
- * Dependency Invertion Principle
- */
-
 class CreateAppointmentService {
-  private appointmentsRepository: AppointmentsRepository;
+  public async execute({ date, provider }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
-  constructor(appointmentsRepository: AppointmentsRepository) {
-    this.appointmentsRepository = appointmentsRepository;
-  }
-
-  public execute({ date, provider }: Request): Appointment {
     const appoitmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
       date,
     );
 
@@ -33,10 +23,12 @@ class CreateAppointmentService {
       throw new Error('This appointment is already booked');
     }
 
-    const appointment = this.appointmentsRepository.create({
+    const appointment = appointmentsRepository.create({
       provider,
       date: appoitmentDate,
     });
+
+    await appointmentsRepository.save(appointment);
 
     return appointment;
   }
