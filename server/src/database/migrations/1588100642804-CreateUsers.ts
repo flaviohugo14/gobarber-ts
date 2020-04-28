5fa27ec3964c4c4e579ab59c81583e4693651897
Flávio Pangrácio <flaviohugo.14@gmail.com>
2020-04-28 16:21:03 -0300

    model: Create model of Users

--- server/src/database/migrations/1588100642804-CreateUsers.ts
+++ server/src/database/migrations/1588100642804-CreateUsers.ts
@@ -24,6 +24,10 @@ export default class CreateUsers1588100642804 implements MigrationInterface {
             isUnique: true,
           },
           {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
             name: 'created_at',
             type: 'timestamp',
             default: 'now()',
