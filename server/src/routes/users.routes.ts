836b8dae42517ced8c53b53f687c6ed5e67c9e11
Flávio Pangrácio <flaviohugo.14@gmail.com>
2020-04-30 16:41:33 -0300

    feat: dealt global of errors

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -11,23 +11,19 @@ const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService();
+  const createUser = new CreateUserService();
 
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
-    });
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
 
-    delete user.password;
+  delete user.password;
 
-    return response.json(user);
-  } catch (err) {
-    return response.status(err.statusCode).json({ error: err.message });
-  }
+  return response.json(user);
 });
 
 usersRouter.patch(
@@ -35,23 +31,19 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const { id } = request.user;
-      const { filename } = request.file;
+    const { id } = request.user;
+    const { filename } = request.file;
 
-      const updateUserAvatar = new UpdateUserAvatarService();
+    const updateUserAvatar = new UpdateUserAvatarService();
 
-      const user = await updateUserAvatar.execute({
-        user_id: id,
-        avatarFileName: filename,
-      });
+    const user = await updateUserAvatar.execute({
+      user_id: id,
+      avatarFileName: filename,
+    });
 
-      delete user.password;
+    delete user.password;
 
-      return response.json(user);
-    } catch (err) {
-      return response.status(err.statusCode).json({ error: err.message });
-    }
+    return response.json(user);
   },
 );
 
