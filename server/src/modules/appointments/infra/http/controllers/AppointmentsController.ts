2517d577a72831afc6bd6379198f47bfcedb8f5e
Flávio Pangrácio <flaviohugo.14@gmail.com>
2020-05-06 17:15:29 -0300

    feat: add Jest for tests

--- server/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ server/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { parseISO } from 'date-fns';
 import { container } from 'tsyringe';
 
-import CreateAppointmentService from '@modules/appointments/services/CreateAppoitmentService';
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 class AppointmentsController {
   public async create(request: Request, response: Response): Promise<Response> {
---
+++ server/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,23 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '121212',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('121212');
+  });
+
+  // it('should not be able to create two appointments on the same time', () => {
+
+  // })
+});
